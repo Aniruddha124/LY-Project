8a549e4669e9a5e7257cf0fe078bdb6f25217812
AxonBlAzE <s.mulkutkar@somaiya.edu>
2022-11-26 10:05:50 +0530

    setup for fetch

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import { TextInput, Input, Tooltip, Center, Button } from '@mantine/core';
 import { IconCurrencyBitcoin } from "@tabler/icons";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+
+  const [address, setAddress] = useState("")
+
+  const handleSearch = () => {
+    console.log("searching for address: " + address)
+  }
+
   return (
     <div style={{
       display: 'flex',
@@ -9,14 +17,15 @@ export default function Home() {
       alignItems: 'center',
       height: '70vh',
     }}>
-      <Center style={{ width: 1000, height: 200 }}>
+      <Center>
         <Input
-        icon={<IconCurrencyBitcoin size={16} />}
+          icon={<IconCurrencyBitcoin size={16} />}
           style={{ width: 800, height: 60 }}
           size='xl'
           placeholder="Enter a Public Address"
+          onChange={(e) => setAddress(e.target.value)}
         />
-        <Button style={{ width: 200, height: 60 }} size='xl' variant='filled'>
+        <Button style={{ width: 200, height: 60 }} size='xl' variant='filled' onClick={handleSearch}>
           Search
         </Button>
       </Center>

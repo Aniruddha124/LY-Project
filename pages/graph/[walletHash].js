1a2a2f86133cc758a67df7a0ec4312d7a7ca2075
Shreyans <s.mulkutkar@somaiya.edu>
2023-04-15 19:59:32 +0530

    made /graph dynamic

--- pages/graph/[walletHash].js
+++ pages/graph/[walletHash].js
@@ -1,49 +1,39 @@
 import React, { useState, useEffect } from "react";
 import Graph from "react-graph-vis";
+import { useRouter } from "next/router";
 
 
 export default function Graph_Test() {
 
+    const router = useRouter();
+    const { walletHash } = router.query;
     const [hydrated, setHydrated] = useState(false);
+    const [graph, setGraph] = useState([]);
     
     useEffect(() => {
-        setHydrated(true);
-    },[])
+        async function fetchData() {
+        if (walletHash != undefined)
+            try {
+                console.log("walletHash: ", walletHash)
 
-    const graph = {
-        "nodes": [
-          {
-            "id": 1,
-            "label": "19snqSYnDSC4mDbv3pJuYgYqm5ctqwAxnm",
-            "balance": 1.0
-          },
-          {
-            "id": 2,
-            "label": "21snqSYnDSC4mDbv3pJuYgYqm5ctqwAxnm",
-            "balance": 1.0
-          },
-          {
-            "id": 3,
-            "label": "20snqSYnDSC4mDbv3pJuYgYqm5ctqwAxnm",
-            "balance": 2.0
-          }
-        ],
-        "edges": [
-          {
-            "from": 1,
-            "to": 2
-          },
-          {
-            "from": 1,
-            "to": 3
-          },
-          {
-            "from": 1,
-            "to": 1
-          }
-        ]
-      };
-    
+                const response = await fetch(
+                    `http://127.0.0.1:5000/fetch_node/${walletHash}`
+                );
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                const data = await response.json();
+                console.log(data);
+                setGraphData(data);
+                setHydrated(true);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        // setHydrated(true);
+        fetchData();
+    },[walletHash])
+
     const options = {
         layout: {
             hierarchical: false,
